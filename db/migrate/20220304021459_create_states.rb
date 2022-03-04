cf3cf8c3ac770720c91cf7c7d37445e10e9b8983
Higor <hhigorsilvachaves@gmail.com>
2022-03-04 00:08:42 -0300

    Add city and state models

---
+++ db/migrate/20220304021459_create_states.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateStates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :states do |t|
+      t.string :name
+      t.string :uf
+
+      t.timestamps
+    end
+  end
+end
