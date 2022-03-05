854663935730e8032dbef807b2a037427b999d8e
Higor <hhigorsilvachaves@gmail.com>
2022-03-05 14:49:27 -0300

    Add profile model

---
+++ db/migrate/20220307020716_create_profiles.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CreateProfiles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :profiles do |t|
+      t.string :first_name
+      t.string :last_name
+      t.datetime :born
+      t.text :description
+      t.references :city, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
