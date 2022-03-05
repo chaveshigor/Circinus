4788559d6e3cf4e85af250a69265fa4bd40be8a0
Higor <hhigorsilvachaves@gmail.com>
2022-03-05 19:26:47 -0300

    Remove find rj in test cuz is not part of the test scope

--- spec/requests/api/states_spec.rb
+++ spec/requests/api/states_spec.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'json'
 
 RSpec.describe 'Api::States', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'show all states' do
+      get '/api/states'
+
+      all_states = JSON.parse(response.body)
+      expect(all_states.count).to eq(27)
+    end
   end
 end
