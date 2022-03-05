3c7b13a29b8dadd4bc7b77c56a7ec4a222b83c62
Higor <hhigorsilvachaves@gmail.com>
2022-03-05 19:56:02 -0300

    Add data to sanitaze method and get the user full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   has_one :profile
   validates :email, presence: true, format: { with: /\A[a-zA-Z_0-9-]{1,}@[a-zA-Z.]{1,}\z/, message: 'invalid email' }
   validates :password_digest, presence: true, length: { minimum: 8 }
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+
+  def full_name
+    "#{first_name.titleize} #{last_name.titleize}"
+  end
 
   private
 
@@ -14,5 +20,7 @@ class User < ApplicationRecord
 
   def sanitaze
     self.email = email.downcase
+    self.first_name = first_name.downcase
+    self.last_name = last_name.downcase
   end
 end
