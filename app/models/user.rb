dfba3853490db8c226db43fe18faf1f65077d21d
Higor <hhigorsilvachaves@gmail.com>
2022-03-05 16:51:24 -0300

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_create :set_hash_pass, :sanitaze
   has_one :profile
+  validates :email, presence: true, format: { with: /\A[a-zA-Z_0-9-]{1,}@[a-zA-Z.]{1,}+\z/, message: 'invalid email' }
+  validates :password_digest, presence: true, length: { minimum: 8 }
+
+  private
+
+  def set_hash_pass
+    self.password_digest = BCrypt::Password.create(password_digest)
+  end
+
+  def sanitaze
+    self.email = email.downcase
+  end
 end
