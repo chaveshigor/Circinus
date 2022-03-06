7c38946ec4cd950e24bdb73a428739336903e779
Higor <hhigorsilvachaves@gmail.com>
2022-03-06 16:27:30 -0300

    Add action to check user authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def authorize_request
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(token)
+      @current_user = User.find(@decoded[:data])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { status: 'failed', message: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { status: 'failed', message: e.message }, status: :unauthorized
+    end
+  end
 end
