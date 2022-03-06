d8ba049728b40944f0845680cbbd2a2173d3e859
Higor <hhigorsilvachaves@gmail.com>
2022-03-05 22:59:03 -0300

    Add first name and last name

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,7 +2,9 @@
 
 FactoryBot.define do
   factory :user do
-    email { 'john@example.com' }
+    first_name { 'eren' }
+    last_name { 'yeager' }
+    email { "#{first_name}_#{last_name}@example.com" }
     password_digest { 'P4ssW02d!' }
   end
 end
