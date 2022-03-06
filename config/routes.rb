b5c9e0b769c1a47f743ae19139dae06d79f82673
Higor <hhigorsilvachaves@gmail.com>
2022-03-06 18:05:00 -0300

    Add profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,14 @@ Rails.application.routes.draw do
       delete 'registration', to: 'registration#destroy'
       put 'registration/:user_id/:confirmation_token', to: 'registration#confirmate_account'
       post 'auth', to: 'auth#create'
-      #resources :auth
+      # resources :auth
     end
 
     get 'states', to: 'states#index'
     get 'states/:id/cities', to: 'cities#index'
     get 'cities/:id', to: 'cities#show'
+
+    resources :profiles
   end
   root 'pages#index'
   get '*path', to: 'pages#index'
