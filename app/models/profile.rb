2b1bfaf74fab8545fc8bcfc5ce37a7938f1342ea
Higor <hhigorsilvachaves@gmail.com>
2022-03-06 21:11:17 -0300

    Add method to return age

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,14 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_one :city
+
+  def calculate_age
+    (Date.today - Date.new(born.year, born.month, born.day)).to_i / 365
+  end
+
+  def send_profile
+    profile_hash = attributes
+    profile_hash['age'] = calculate_age
+    profile_hash
+  end
 end
