1b5129dfa9848ec1d9f0a4f2e24fc2196e750b7b
Higor <hhigorsilvachaves@gmail.com>
2022-03-07 20:48:09 -0300

    Add validation and full name to send_profile method

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_one :city
+  validates :description, presence: true, length: { minimum: 16 }
 
   def calculate_age
     (Date.today - Date.new(born.year, born.month, born.day)).to_i / 365
@@ -11,6 +12,7 @@ class Profile < ApplicationRecord
   def send_profile
     profile_hash = attributes
     profile_hash['age'] = calculate_age
+    profile_hash['full_name'] = user.full_name
     profile_hash
   end
 end
