2f904b0f099e4c3b1008ada4015aa66eedc2f8d2
Higor <hhigorsilvachaves@gmail.com>
2022-03-06 21:12:42 -0300

    Add pprofile controller

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,2 +1,30 @@
 class Api::ProfilesController < ApplicationController
+  before_action :authorize_request
+
+  def index
+    current_user_city = City.find(@current_user.profile.city_id)
+    profiles = Profile.where({ city_id: current_user_city.id })
+    profiles.delete(@current_user.profile)
+    render json: { status: 'success', profiles: profiles }
+  end
+
+  def create
+    city = City.find(profile_params[:city_id]) rescue user = nil
+    return render json: { status: 'failed', message: 'city not found' } if city.nil?
+
+    profile = Profile.new(profile_params)
+    profile.user_id = @current_user.id
+    profile.save
+    render json: { status: 'success', profile: profile.send_profile }
+  end
+
+  def update
+
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:born, :description, :city_id)
+  end
 end
