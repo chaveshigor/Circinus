58a4031e8c2abf92e95f2125371c0eff79f4e7bf
Higor <hhigorsilvachaves@gmail.com>
2022-03-12 18:53:49 -0300

    Add profiles controller

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,17 +1,14 @@
 class Api::ProfilesController < ApplicationController
   before_action :authorize_request
+  before_action :check_city, only: %i[create update]
 
   def index
     current_user_city = City.find(@current_user.profile.city_id)
-    profiles = Profile.where({ city_id: current_user_city.id })
-    profiles.delete(@current_user.profile)
+    profiles = Profile.where({ city_id: current_user_city.id }).where.not({ user_id: @current_user.id })
     render json: { status: 'success', profiles: profiles.map(&:send_profile) }
   end
 
   def create
-    city = City.find(profile_params[:city_id]) rescue user = nil
-    return render json: { status: 'failed', message: 'city not found' } if city.nil?
-
     profile = Profile.new(profile_params)
     profile.user_id = @current_user.id
     begin
@@ -23,12 +20,25 @@ class Api::ProfilesController < ApplicationController
   end
 
   def update
+    profile = Profile.find(params[:id])
+    profile.update(profile_params_edit)
 
+    render json: { status: 'success', profile: profile }
   end
 
   private
 
+  def check_city
+    City.find(profile_params[:city_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { status: 'failed', message: 'city not found' }
+  end
+
   def profile_params
     params.require(:profile).permit(:born, :description, :city_id)
+  end
+
+  def profile_params_edit
+    params.require(:profile_edit).permit(:id, :description, :city_id)
   end
 end
