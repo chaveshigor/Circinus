111ca5f74061b8f62029ec2177384290376282d5
Higor <hhigorsilvachaves@gmail.com>
2022-03-13 21:02:16 -0300

    Add many to many relation between profile and hobby

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
   belongs_to :city
+  has_many :profile_hobbies
+  has_many :hobbies, through: :profile_hobbies
   validates :description, presence: true, length: { minimum: 16 }
 
   def calculate_age
