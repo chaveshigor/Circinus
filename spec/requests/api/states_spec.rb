0ae21b6852c6693ccc475cecba911a0a0622b1bf
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-03-28 21:11:17 -0300

    Refactor, add context to test

--- spec/requests/api/states_spec.rb
+++ spec/requests/api/states_spec.rb
@@ -8,16 +8,18 @@ RSpec.describe 'Api::States', type: :request do
   let!(:state3) { create(:state, name: 'Kings Landing', uf: 'KL') }
 
   describe 'GET /index' do
-    it 'show all states' do
-      get '/api/states'
-
-      all_states = JSON.parse(response.body)
-      number_of_states = State.all.count
-
-      expect(all_states.count).to eq(number_of_states)
-      expect(State.find(state1.id)).to eq(state1)
-      expect(State.find(state2.id)).to eq(state2)
-      expect(State.find(state3.id)).to eq(state3)
+    context 'When get all cities' do
+      it 'show all states' do
+        get '/api/states'
+  
+        all_states = JSON.parse(response.body)
+        number_of_states = State.all.count
+  
+        expect(all_states.count).to eq(number_of_states)
+        expect(State.find(state1.id)).to eq(state1)
+        expect(State.find(state2.id)).to eq(state2)
+        expect(State.find(state3.id)).to eq(state3)
+      end
     end
   end
 end
