181d8ca0e767a55ca301fc1f38c55192779b3b2b
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-04-22 00:39:41 -0300

    Add configuration to upload files to s3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+p ENV['AWS_ACCESS_KEY_ID']
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:              'AWS',                         # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],      # required unless using use_iam_profile
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],  # required unless using use_iam_profile
+    region:                ENV['AWS_REGION'],             # optional, defaults to 'us-east-1'
+  }
+  config.fog_provider   = "fog/aws"
+  config.storage        = :fog
+  config.fog_directory  = ENV['S3_BUCKET_NAME']                                 # required
+  config.fog_public     = false                                                 # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+end
