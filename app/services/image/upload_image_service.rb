5bed3dd63605b2e01055e0fa46f439def0cd1489
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-04-22 00:40:20 -0300

    Add service to upload picture

---
+++ app/services/image/upload_image_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Image::UploadImageService < ApplicationService
+  def initialize(image)
+    @image = image
+  end
+
+  def run
+    uploadImage
+  end
+
+  private
+
+  attr_reader :image
+
+  def uploadImage
+    uploader = PictureUploader.new
+    uploader.store!(image)
+    uploader.url
+  end
+end
