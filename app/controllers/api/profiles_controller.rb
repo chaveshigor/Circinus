9d03e4769d551e26cdb1e2c1c5ef2b210ce0d89d
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-04-22 23:46:34 -0300

    Add services to update pictures

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -5,29 +5,34 @@ class Api::ProfilesController < Api::ApiController
   def index
     current_user_city = City.find(@current_user.profile.city_id)
     profiles = Profile.where({ city_id: current_user_city.id }).where.not({ user_id: @current_user.id })
+
     render json: { status: 'success', profiles: Api::ProfileSerializer.new(profiles).serializable_hash }
   end
 
   def update
-    profile = Profile.find(params[:id])
-    profile.update(edit_profile_params)
+    begin
+      profile = Profile.find(@current_user.profile.id)
+      profile.update(edit_profile_params)
 
-    render json: { status: 'success', profile: profile }
+      ProfileImages::AddPicturesService.new(params[:add_pictures], profile).run if params[:add_pictures].present?
+      ProfileImages::DestroyPicturesService.new(params[:destroy_pictures]).run  if params[:destroy_pictures].present?
+      ProfileImages::MovePicturesService.new(params[:move_pictures]).run        if params[:move_pictures].present?
+    rescue ActiveRecord::RecordNotFound => e
+      return not_found_request(e.message)
+    end
+
+    render json: { status: 'success', profile: Api::ProfileSerializer.new(profile).serializable_hash[:data][:attributes] }
   end
 
   private
 
   def check_city
-    City.find(profile_params[:city_id])
+    City.find(edit_profile_params[:city_id])
   rescue ActiveRecord::RecordNotFound
-    render json: { status: 'failed', message: 'city not found' }
-  end
-
-  def profile_params
-    params.require(:profile).permit(:born, :description, :city_id)
+    not_found_request('city not found')
   end
 
   def edit_profile_params
-    params.require(:profile_edit).permit(:id, :description, :city_id)
+    params.require(:profile_edit).permit(:description, :city_id)
   end
 end
