8cdaadee56c493722f5bd74ff084cd1d314f28d2
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-04-22 23:47:29 -0300

    Add profile serializer

--- app/serializers/api/profile_serializer.rb
+++ app/serializers/api/profile_serializer.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class Api::ProfileSerializer < AplicationSerializer
-  attributes :id, :born, :description, :city_id, :user_id, :pictures
+  attributes :id, :description, :city_id, :user_id, :pictures
+  
+  attribute :age do |object|
+    object.calculate_age
+  end
+
+  attribute :full_name do |object|
+    object.user.full_name
+  end
 end
