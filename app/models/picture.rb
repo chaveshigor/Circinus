4f802ff3da14ca4bc762d38c5ec5bf8d3bc82e40
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-04-22 23:47:03 -0300

    Add mount file

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+require 'carrierwave/orm/activerecord'
+
 class Picture < ApplicationRecord
+  mount_uploader :picture_s3, PictureUploader
   belongs_to :profile
 end
