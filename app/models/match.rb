e35916b93785f2639300c483c3844fbf743f5915
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-04-26 22:24:26 -0300

    Add matches scope

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,6 @@
 class Match < ApplicationRecord
   belongs_to :profile_1, class_name: :Profile
   belongs_to :profile_2, class_name: :Profile
+
+  scope :profile_matches, -> (current_profile_id) { Match.select('id', 'profile_1_id', 'profile_2_id').where(profile_1_id: current_profile_id).or(Match.where(profile_2_id: current_profile_id)) }  
 end
