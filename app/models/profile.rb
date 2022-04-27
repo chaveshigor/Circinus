cb8bbe0e480ff86ce5a85accde1e6fcc13f04dbf
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-04-26 22:23:50 -0300

    Add user and city validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,16 +6,13 @@ class Profile < ApplicationRecord
   has_many :pictures
   has_many :profile_hobbies
   has_many :hobbies, through: :profile_hobbies
+
+  validates_presence_of :user
+  validates_presence_of :city
+
   validates :description, presence: true, length: { minimum: 16 }
 
   def calculate_age
     (Date.today - Date.new(born.year, born.month, born.day)).to_i / 365
-  end
-
-  def send_profile
-    profile_hash = attributes
-    profile_hash['age'] = calculate_age
-    profile_hash['full_name'] = user.full_name
-    profile_hash
   end
 end
