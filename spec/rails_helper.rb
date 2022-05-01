d30bf747ab7418c953d20e1a3bd3337db80206d8
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-05-01 12:57:49 -0300

    Add shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 require 'rspec/rails'
 require './spec/support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
