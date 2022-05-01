89935967a03361749c9893fe966653f8599e30bc
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-05-01 12:50:52 -0300

    Add validations to picture model tests

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Picture, type: :model do
   describe 'Assossiations' do
     it { should belong_to(:profile) }
   end
+
+  describe "Validations" do
+    it { should validate_presence_of(:position) }
+    it { should validate_presence_of(:storage_service_key) }
+  end
 end
