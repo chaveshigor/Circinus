6dfca86b90dd7c0a2ba2cb113c9e507b5c0cb855
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-05-06 21:49:09 -0300

    Add dislike model tests

---
+++ spec/models/dislike_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Dislike, type: :model do
+  describe 'Assossiations' do
+    it { should belong_to(:profile_sender) }
+    it { should belong_to(:profile_receiver) }
+  end
+end
